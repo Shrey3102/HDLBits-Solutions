72d1ad30ef2f55665fdd7fe4ca3ed516f9fd3139
Shreyas Channeshappa Shetty <127424978+Shrey3102@users.noreply.github.com>
2026-01-14 19:46:09 +0530

    Update 20_Modules.v

--- 1. Verilog Language/1.3  Modules Hierarchy/20_Modules.v
+++ 1. Verilog Language/1.3  Modules Hierarchy/20_Modules.v
@@ -1,1 +1,5 @@
-
+module top_module ( input a, input b, output out );
+    mod_a t1(.in1(a), .in2(b), .out(out));
+    // Alternative way to instantiate a module
+    // mod_a t1(a, b, out);
+endmodule
